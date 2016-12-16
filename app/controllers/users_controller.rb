15203ef7927abb445105ebd0da6449c3fd4bc860
Noriaki Ibe <aqrcity@gmail.com>
2016-12-16 09:57:15 +0000

    some-error-fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
  def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    #@microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -22,7 +22,19 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user , notice: 'Updated!'
+    
+    else
+      render 'edit'
+    end
+  end
+  
+ 
+  
   private
 
   def user_params
